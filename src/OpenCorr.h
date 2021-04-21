eaf1f87be490d0d35ca02eab83cd0b4d8d5515d3
Zhenyu Jiang <13809474@qq.com>
2021-04-21 11:40:53 +0800

    Create OpenCorr.h
    head file for building of executable program

---
+++ src/OpenCorr.h
@@ -0,0 +1,38 @@
+/*
+ * This file is part of OpenCorr, a open-source C++ library for
+ * development of 2D, 3D/stereo and volumetric digital image correlation.
+ *
+ * Copyright (C) 2021, Zhenyu Jiang <zhenyujiang@scut.edu.cn>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * More information about OpenCorr can be found at http://www.opencorr.org/
+*/
+
+#pragma once
+
+#ifndef _OPENCORR_
+#define _OPENCORR_
+
+#include "oc_array.h"
+#include "oc_bicubic_bspline.h"
+#include "oc_calibration.h"
+#include "oc_deformation.h"
+#include "oc_dic.h"
+#include "oc_feature_affine.h"
+#include "oc_fftcc.h"
+#include "oc_gradient.h"
+#include "oc_icgn.h"
+#include "oc_image.h"
+#include "oc_interpolation.h"
+#include "oc_io.h"
+#include "oc_poi.h"
+#include "oc_point.h"
+#include "oc_sift.h"
+#include "oc_stereovision.h"
+#include "oc_subset.h"
+#include "oc_strain.h"
+
+#endif //_OPENCORR_
