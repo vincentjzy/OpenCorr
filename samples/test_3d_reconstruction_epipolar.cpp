3ab6c0dc64c715b5a1957a7d7f3f54499e6f3098
vincentjzy <13809474@qq.com>
2021-11-19 19:48:32 +0800

    update test_3d_reconstruction_epipolar.cpp with a new sample

--- samples/test_3d_reconstruction_epipolar.cpp
+++ samples/test_3d_reconstruction_epipolar.cpp
@@ -1,5 +1,5 @@
 /*
- This example demonstrates how to use OpenCorr to perform stereo matching of 
+ This example demonstrates how to use OpenCorr to perform stereo matching of
  points in two camera views, using the epipolar constraint aided method and
  the ICGN algorithm with the 2nd order shape function.
 */
@@ -11,19 +11,12 @@ using namespace std;
 int main() {
 	//set paths of images
 	//in this example, the right image of initial state is used as the reference image
-	string view1_image_path = "../samples/GT4-0273_0.tif";
-	string view2_image_path = "../samples/GT4-0273_1.tif";
+	string view1_image_path = "../samples/Step18 00,00-0005_0.tif";
+	string view2_image_path = "../samples/Step18 00,00-0005_1.tif";
 
 	//create the instances of images
 	Image2D view1_img(view1_image_path);
 	Image2D view2_img(view2_image_path);
-
-	//set path of csv file that contains the coordinates of POIs
-	string file_path = "../samples/GT4-POIs.csv";
-	//create an instance to read and write csv files
-	IO2D in_out;
-	in_out.setPath(file_path);
-	in_out.setDelimiter(string(","));
 
 	//get time of start
 	double timer_tic = omp_get_wtime();
@@ -36,14 +29,14 @@ int main() {
 	//create the instances of camera parameters
 	CameraIntrinsics view1_cam_intrinsics, view2_cam_intrinsics;
 	CameraExtrinsics view1_cam_extrinsics, view2_cam_extrinsics;
-	view1_cam_intrinsics.fx = 6673.3159f;
-	view1_cam_intrinsics.fy = 6669.3027f;
+	view1_cam_intrinsics.fx = 10664.80664f;
+	view1_cam_intrinsics.fy = 10643.88965f;
 	view1_cam_intrinsics.fs = 0.f;
-	view1_cam_intrinsics.cx = 872.15778f;
-	view1_cam_intrinsics.cy = 579.95532f;
-	view1_cam_intrinsics.k1 = 0.03225895f;
-	view1_cam_intrinsics.k2 = -1.0114142f;
-	view1_cam_intrinsics.k3 = 29.788389f;
+	view1_cam_intrinsics.cx = 1176.03418f;
+	view1_cam_intrinsics.cy = 914.7337036f;
+	view1_cam_intrinsics.k1 = 0.030823536f;
+	view1_cam_intrinsics.k2 = -1.350255132f;
+	view1_cam_intrinsics.k3 = 74.21749878f;
 	view1_cam_intrinsics.k4 = 0;
 	view1_cam_intrinsics.k5 = 0;
 	view1_cam_intrinsics.k6 = 0;
@@ -57,26 +50,26 @@ int main() {
 	view1_cam_extrinsics.ry = 0;
 	view1_cam_extrinsics.rz = 0;
 
-	view2_cam_intrinsics.fx = 6607.6182f;
-	view2_cam_intrinsics.fy = 6602.8574f;
+	view2_cam_intrinsics.fx = 10749.53223f;
+	view2_cam_intrinsics.fy = 10726.52441f;
 	view2_cam_intrinsics.fs = 0.f;
-	view2_cam_intrinsics.cx = 917.97339f;
-	view2_cam_intrinsics.cy = 531.63525f;
-	view2_cam_intrinsics.k1 = 0.06459849f;
-	view2_cam_intrinsics.k2 = -4.531374f;
-	view2_cam_intrinsics.k3 = 29.788389f;
+	view2_cam_intrinsics.cx = 1034.707886f;
+	view2_cam_intrinsics.cy = 1062.162842f;
+	view2_cam_intrinsics.k1 = 0.070953421f;
+	view2_cam_intrinsics.k2 = -4.101067066f;
+	view2_cam_intrinsics.k3 = 74.21749878f;
 	view2_cam_intrinsics.k4 = 0;
 	view2_cam_intrinsics.k5 = 0;
 	view2_cam_intrinsics.k6 = 0;
 	view2_cam_intrinsics.p1 = 0;
 	view2_cam_intrinsics.p2 = 0;
 
-	view2_cam_extrinsics.tx = 122.24886f;
-	view2_cam_extrinsics.ty = 1.8488892f;
-	view2_cam_extrinsics.tz = 17.624638f;
-	view2_cam_extrinsics.rx = 0.00307711f;
-	view2_cam_extrinsics.ry = -0.33278773f;
-	view2_cam_extrinsics.rz = 0.00524556f;
+	view2_cam_extrinsics.tx = 250.881488962793f;
+	view2_cam_extrinsics.ty = -1.15469183120196f;
+	view2_cam_extrinsics.tz = 37.4849858174401f;
+	view2_cam_extrinsics.rx = 0.01450813f;
+	view2_cam_extrinsics.ry = -0.39152833f;
+	view2_cam_extrinsics.rz = 0.01064092f;
 
 	//create the instances for stereovision
 	Calibration cam_view1_calib(view1_cam_intrinsics, view1_cam_extrinsics);
@@ -85,38 +78,43 @@ int main() {
 	cam_view2_calib.prepare(view2_img.height, view2_img.width);
 	Stereovision stereo_reconstruction(&cam_view1_calib, &cam_view2_calib, cpu_thread_number);
 
-	//load the coordinates of POIs in principal view
-	vector<Point2D> view1_pt_queue = in_out.loadPOI2D();
-	int queue_length = (int)view1_pt_queue.size();
+	//set POIs
+	Point2D upper_left_point(420, 250);
+	vector<Point2D> view1_pt_queue;
+	vector<POI2D> poi_queue; //POI for matching
+	vector<POI2DS> poi_result_queue; //POI used to store the results
+
+	int poi_number_x = 313;
+	int poi_number_y = 313;
+	int grid_space = 5;
+
+	//store POIs in a queue
+	for (int i = 0; i < poi_number_y; i++) {
+		for (int j = 0; j < poi_number_x; j++) {
+			Point2D offset(j * grid_space, i * grid_space);
+			Point2D current_point = upper_left_point + offset;
+			view1_pt_queue.push_back(current_point);
+
+			POI2D current_poi(current_point);
+			poi_queue.push_back(current_poi);
+
+			POI2DS current_poi_2ds(current_point);
+			poi_result_queue.push_back(current_poi_2ds);
+		}
+	}
 
 	//create the the queues of 2D points for stereo matching and reconstruction
+	int queue_length = (int)view1_pt_queue.size();
 	Point2D point_2d;
 	vector<Point2D> view2_pt_queue(queue_length, point_2d);
 
 	//create the the queues of 3D points for stereo reconstruction
 	Point3D point_3d;
 	vector<Point3D> pt_3d_queue(queue_length, point_3d);
-	//assign the coordinates to a queue of POIs
-
-	//POI for matching
-	vector<POI2D> poi_queue;
-	//POI used to store the results
-	vector<POI2DS> poi_result_queue;
-
-	//assign the queues of POIs with coordinates
-	for (int i = 0; i < queue_length; i++) {
-		Point2D current_point = view1_pt_queue[i];
-
-		POI2D current_POI(current_point);
-		poi_queue.push_back(current_POI);
-
-		POI2DS current_poi_2ds(current_point);
-		poi_result_queue.push_back(current_poi_2ds);
-	}
 
 	//create the instances of ICGN
-	int subset_radius_x = 16;
-	int subset_radius_y = 16;
+	int subset_radius_x = 9;
+	int subset_radius_y = 9;
 	float conv_criterion = 0.001f;
 	int stop_condition = 10;
 
@@ -129,13 +127,13 @@ int main() {
 	//set search parameters in epipolar constraint aided matching
 	Point2D parallax_guess(-30, -40);
 	epipolar_search->setParallax(parallax_guess);
-	int search_radius = 30;
-	int search_step = 5;
+	int search_radius = 150;
+	int search_step = 4;
 	epipolar_search->setSearch(search_radius, search_step);
 
 	//initialize an ICGN2D1 instance in epipolar constraint aided matching
-	subset_radius_x = 20;
-	subset_radius_y = 20;
+	subset_radius_x = 15;
+	subset_radius_y = 15;
 	conv_criterion = 0.05f;
 	stop_condition = 5;
 	epipolar_search->setICGN(subset_radius_x, subset_radius_y, conv_criterion, stop_condition);
@@ -180,9 +178,9 @@ int main() {
 	//store the 3D coordinates for output
 #pragma omp parallel for
 	for (int i = 0; i < poi_queue.size(); i++) {
-		poi_result_queue[i].deformation.u = pt_3d_queue[i].x;
-		poi_result_queue[i].deformation.v = pt_3d_queue[i].y;
-		poi_result_queue[i].deformation.w = pt_3d_queue[i].z;
+		poi_result_queue[i].ref_coor.x = pt_3d_queue[i].x;
+		poi_result_queue[i].ref_coor.y = pt_3d_queue[i].y;
+		poi_result_queue[i].ref_coor.z = pt_3d_queue[i].z;
 	}
 
 	//get the time of end 
@@ -193,12 +191,16 @@ int main() {
 	cout << "Stereo matching and reconstruction: " << consumed_time << " sec" << std::endl;
 
 
+	//create an instance to read and write csv files
+	IO2D in_out;
+	in_out.setDelimiter(string(","));
+
 	//output the results
 	in_out.setHeight(view1_img.height);
 	in_out.setWidth(view1_img.width);
 
 	//save the calculated dispalcements
-	file_path = view2_image_path + "_reconstruction_table.csv";
+	string file_path = view2_image_path + "_reconstruction_table.csv";
 	in_out.setPath(file_path);
 	in_out.saveTable2DS(poi_result_queue);
 
